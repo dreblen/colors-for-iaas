b2cfa668996d1267d3aed52e8a29815db3f8e0e4
Tanner Jotblad <tannerjotblad@gmail.com>
2016-11-05 20:33:50 -0500

    Initial Express App
    -Basic setup for an Express app.

---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Colors for IaaS' });
+});
+
+module.exports = router;

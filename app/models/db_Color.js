fa3cbd47aa63573e7f2a1864f669ab7a347813b4
Tanner Jotblad <tannerjotblad@gmail.com>
2017-01-15 15:26:16 -0600

    Database Initialization -Change DB init mechanism to use environment variables rather than a stored conf file.

--- app/models/db_Color.js
+++ app/models/db_Color.js
@@ -1,7 +1,15 @@
-var config = JSON.parse(require('fs').readFileSync(__dirname + '/db.conf', {encoding:'utf-8'}));
+var config = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost',
+    dialect: (process.env.DB_DIALECT) ? process.env.DB_DIALECT : 'mysql'
+}
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('colors-for-iaas', config.username, config.password);
+var sequelize = new Sequelize('colors-for-iaas', config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect
+});
 
 module.exports = Color = sequelize.define('color', {
     name: Sequelize.STRING,
--- db/init-structure.sh
+++ db/init-structure.sh
@@ -7,7 +7,7 @@ mysqladmin -u root -p create colors-for-iaas
 export USER=cfi_appuser
 export PW=$(</dev/urandom tr -dc 'A-Za-z0-9' | head -c 20
 )
-# Save the password to a conf file for the app
-echo "{\"username\":\"$USER\",\"password\":\"$PW\"}" > ../app/models/db.conf
+# Let us know the password that was created
+echo $PW
 # and use the password for a new database user
 mysql -u root -p -e "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PW'; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON \`colors-for-iaas\`.* TO '$USER'@'localhost'; FLUSH PRIVILEGES;"
